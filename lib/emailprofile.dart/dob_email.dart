f26dee3324c398f1cdd3f9a8221a0df90e6b129b
fawad kaleem <fwdkaleem@gmail.com>
2023-06-15 10:55:22 +0500

    PhotEmail

---
+++ lib/emailprofile.dart/dob_email.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DobEmail extends StatefulWidget {
+  const DobEmail({super.key});
+
+  @override
+  State<DobEmail> createState() => _DobEmailState();
+}
+
+class _DobEmailState extends State<DobEmail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
