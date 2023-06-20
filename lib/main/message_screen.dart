717518ad0f0aaf9af46039c6c5ac4f668ca165c8
fawad kaleem <fwdkaleem@gmail.com>
2023-06-20 13:06:04 +0500

    Message Screen UI Ready

--- lib/main/message_screen.dart
+++ lib/main/message_screen.dart
@@ -1,140 +1,54 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:join/chat_room/current_chat.dart';
+import 'package:join/calenderpages/all.dart';
+import 'package:join/calenderpages/ongoing.dart';
+import 'package:join/calenderpages/past.dart';
+import 'package:join/messages_screen/event_chat.dart';
+import 'package:join/messages_screen/people_chat.dart';
 
-class MessageScreen extends StatefulWidget {
+/// Flutter code sample for [TabBar].
+
+class MessageScreen extends StatelessWidget {
   const MessageScreen({super.key});
 
   @override
-  State<MessageScreen> createState() => _MessageScreenState();
-}
-
-class _MessageScreenState extends State<MessageScreen> {
-  final Stream<QuerySnapshot> _usersStream =
-      FirebaseFirestore.instance.collection('chats').snapshots();
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset("assets/info.png"),
-          )
-        ],
-        title: Text(
-          "Messages",
-          overflow: TextOverflow.ellipsis,
-          textAlign: TextAlign.left,
-          style: TextStyle(
-            color: Color(
-              0xff160F29,
-            ),
-            fontSize: 20,
-            fontWeight: FontWeight.w600,
-            fontFamily: "ProximaNova",
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset("assets/info.png"),
+            )
+          ],
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+          title: Text(
+            "Messages",
+            style: TextStyle(
+                fontFamily: "ProximaNova",
+                fontSize: 20,
+                color: Color(0xff160F29),
+                fontWeight: FontWeight.w600),
+          ),
+          bottom: TabBar(
+            labelColor: Colors.black,
+            tabs: <Widget>[
+              Tab(
+                text: "Events",
+              ),
+              Tab(
+                text: "People",
+              ),
+            ],
           ),
         ),
-      ),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: StreamBuilder(
-                  stream: FirebaseFirestore.instance
-                      .collection("chats")
-                      .where("joinid",
-                          isEqualTo: FirebaseAuth.instance.currentUser!.uid)
-                      .snapshots(includeMetadataChanges: true),
-                  builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (snapshot.hasError) {
-                      return const Center(
-                        child: Text('No Data Found'),
-                      );
-                    }
-
-                    if (snapshot.hasData) {
-                      print(FirebaseAuth.instance.currentUser!.uid);
-
-                      return ListView.builder(
-                          shrinkWrap: true,
-                          physics: ScrollPhysics(),
-                          itemCount: snapshot.data!.docs.length,
-                          itemBuilder: (context, int index) {
-                            final DocumentSnapshot documentSnapshot =
-                                snapshot.data!.docs[index];
-                            return Column(
-                              children: [
-                                InkWell(
-                                    onTap: () {},
-                                    child: ListTile(
-                                      leading: CircleAvatar(
-                                        backgroundImage: NetworkImage(
-                                            documentSnapshot['eventimage']),
-                                      ),
-                                      title: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                              documentSnapshot['eventname']
-                                                  .toString(),
-                                              style: TextStyle(
-                                                  color: Color(0xff858585),
-                                                  fontSize: 14,
-                                                  fontWeight: FontWeight.w400)),
-                                          Text(
-                                            documentSnapshot['joinname']
-                                                .toString(),
-                                            style: TextStyle(
-                                                color: Colors.black,
-                                                fontSize: 14,
-                                                fontWeight: FontWeight.w400),
-                                          ),
-                                        ],
-                                      ),
-                                      trailing: TextButton(
-                                        child: Text(
-                                          "Chat Now",
-                                          style: TextStyle(
-                                              color: Color(0xff246A73)),
-                                        ),
-                                        onPressed: () {
-                                          Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                              builder: (context) =>
-                                                  CurrentChatRoom(
-                                                doctorName: documentSnapshot[
-                                                    'joinname'],
-                                                receiverId:
-                                                    documentSnapshot['eventid'],
-                                                doctorId:
-                                                    documentSnapshot['joinid'],
-                                                receiverName: documentSnapshot[
-                                                    'eventname'],
-                                              ),
-                                            ),
-                                          );
-                                        },
-                                      ),
-                                    )),
-                                Divider()
-                              ],
-                            );
-                          });
-                    } else {
-                      return Center(
-                          child: CircularProgressIndicator.adaptive());
-                    }
-                  }),
-            )
+        body: TabBarView(
+          children: <Widget>[
+            EventsChat(),
+            PeopleChat(),
           ],
         ),
       ),
